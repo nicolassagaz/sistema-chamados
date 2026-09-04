97956bdf4785d6f3fe9f71b4ec184f109b4abbf3
Nicolas Rosa <nsagazrosa@gmail.com>
2026-09-04 10:56:01 -0300

    feat: add endpoit to listo tickets

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,37 @@ from fastapi import FastAPI
 #cria aplicação
 app = FastAPI()
 
+chamados = [
+    {
+        "id": 1010,
+        "titulo": "Computador não liga",
+        "descricao": "Equipamento não inicia",
+        "prioridade": "Alta",
+        "status": "Aberto",
+        "solicitante": "João",
+        "tecnico": None,
+        "data": "01/09/2026"
+    },
+    {
+        "id": 1011,
+        "titulo": "Notebook sem imagem",
+        "descricao": "Tela permanece preta ao ligar",
+        "prioridade": "Alta",
+        "status": "Fechado",
+        "solicitante": "Maria",
+        "tecnico": None,
+        "data": "02/09/2026"
+    }
+]
+
 #executa função
 @app.get("/")
 #retorna dicionário python que o fastapi transforma em json
 def inicio():
     return {"mensagem": "Sistema de chamados funcionando"}
+
+
+@app.get("/chamados")
+def listar_chamados():
+    return chamados
+
